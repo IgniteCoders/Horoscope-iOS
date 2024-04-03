49f6a087ec9015febea9bbf58e7bbedec2ce6d28
Mansour <theboom.kajas@gmail.com>
2024-04-03 12:30:19 +0200

    Initial Commit

---
+++ Horoscope-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Horoscope-iOS
+//
+//  Created by Mañanas on 3/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
